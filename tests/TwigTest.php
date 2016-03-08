slimphp/Twig-View

commit 09eca0e28c7763938a450fa322691fce3d119319

Add the ability to add multiple paths with NS
Allows an array of paths with the key as a namespace. Will
detect if the type is a string and will add the path of the
template as before to not break usage.

src/Twig.php
@@ -50,12 +50,15 @@ class Twig implements \ArrayAccess
     /**
      * Create new Twig view
      *
-     * @param string $path     Path to templates directory
-     * @param array  $settings Twig environment settings
+     * @param string|array $path     Path(s) to templates directory
+     * @param array        $settings Twig environment settings
      */
     public function __construct($path, $settings = [])
     {
-        $this->loader = new \Twig_Loader_Filesystem($path);
+        $this->loader = is_string($path)
+            ? new \Twig_Loader_Filesystem($path)
+            : $this->addPaths($path);
+
         $this->environment = new \Twig_Environment($this->loader, $settings);
     }
 
@@ -102,6 +105,23 @@ class Twig implements \ArrayAccess
          $response->getBody()->write($this->fetch($template, $data));
 
          return $response;
+    }
+
+    /**
+     * Add a selection of paths with the desired namespace
+     *
+     * @param array $paths
+     * @return \Twig_Loader_Filesystem
+     */
+    private function addPaths(array $paths)
+    {
+        $loader = new \Twig_Loader_Filesystem();
+
+        foreach ($paths as $namespace => $path) {
+            $loader->addPath($path, $namespace);
+        }
+
+        return $loader;
     }
 
     /********************************************************************************

tests/TwigTest.php
@@ -33,6 +33,39 @@ class TwigTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("<p>Hi, my name is Josh.</p>\n", $output);
     }
 
+    public function testSingleTemplateWithANamespace()
+    {
+        $views = new Twig([
+            'One' => __DIR__.'/templates',
+        ]);
+
+        $output = $views->fetch('@One/example.html', [
+            'name' => 'Josh'
+        ]);
+
+        $this->assertEquals("<p>Hi, my name is Josh.</p>\n", $output);
+    }
+
+    public function testMultipleTemplatesWithMulNamespace()
+    {
+        $views = new Twig([
+            'One' => __DIR__.'/templates',
+            'Two' => __DIR__.'/another',
+        ]);
+
+        $outputOne = $views->fetch('@One/example.html', [
+            'name' => 'Peter'
+        ]);
+
+        $outputTwo = $views->fetch('@Two/example.html', [
+            'name'   => 'Peter',
+            'gender' => 'male'
+        ]);
+
+        $this->assertEquals("<p>Hi, my name is Peter.</p>\n", $outputOne);
+        $this->assertEquals("<p>Hi, my name is Peter and I am male.</p>\n", $outputTwo);
+    }
+
     public function testRender()
     {
         $mockBody = $this->getMockBuilder('Psr\Http\Message\StreamInterface')

tests/another/example.html
@@ -1,0 +1,1 @@
+<p>Hi, my name is {{ name }} and I am {{ gender }}.</p>

commit fcc832956cc65e94c477c199f4265a770b148540

Fixes #52 - Allow multiple paths per namespace
Fixes the BC break that was introduced with commit 09eca0e.

authors: @NigelGreenway

src/Twig.php
@@ -118,7 +118,7 @@ class Twig implements \ArrayAccess
         $loader = new \Twig_Loader_Filesystem();
 
         foreach ($paths as $namespace => $path) {
-            $loader->addPath($path, $namespace);
+            $loader->setPaths($path, $namespace);
         }
 
         return $loader;

tests/TwigTest.php
@@ -14,29 +14,55 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 class TwigTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var Twig
-     */
-    protected $view;
-
-    public function setUp()
-    {
-        $this->view = new Twig(dirname(__FILE__) . '/templates');
-    }
-
     public function testFetch()
     {
-        $output = $this->view->fetch('example.html', [
+        $view = new Twig(dirname(__FILE__) . '/templates');
+
+        $output = $view->fetch('example.html', [
             'name' => 'Josh'
         ]);
 
         $this->assertEquals("<p>Hi, my name is Josh.</p>\n", $output);
     }
 
+    public function testSingleNamespaceAndMultipleDirectories()
+    {
+        $weekday = (new \DateTimeImmutable('2016-03-08'))->format('l');
+
+        $view = new Twig(
+            [
+                'namespace' => [
+                    __DIR__.'/another',
+                    __DIR__.'/templates',
+                    __DIR__.'/multi',
+                ],
+            ]
+        );
+
+        $anotherDirectory = $view->fetch('@namespace/example.html', [
+            'name' => 'Peter'
+        ]);
+
+        $templatesDirectory = $view->fetch('@namespace/another_example.html', [
+            'name'   => 'Peter',
+            'gender' => 'male',
+        ]);
+
+        $outputMulti = $view->fetch('@namespace/directory/template/example.html', [
+            'weekday' => $weekday,
+        ]);
+
+        $this->assertEquals("<p>Hi, my name is Peter.</p>\n", $anotherDirectory);
+        $this->assertEquals("<p>Hi, my name is Peter and I am male.</p>\n", $templatesDirectory);
+        $this->assertEquals('Happy Tuesday!', $outputMulti);
+    }
+
     public function testSingleTemplateWithANamespace()
     {
         $views = new Twig([
-            'One' => __DIR__.'/templates',
+            'One' => [
+                __DIR__.'/templates',
+            ],
         ]);
 
         $output = $views->fetch('@One/example.html', [
@@ -48,26 +74,38 @@ class TwigTest extends \PHPUnit_Framework_TestCase
 
     public function testMultipleTemplatesWithMulNamespace()
     {
+        $weekday = (new \DateTimeImmutable('2016-03-08'))->format('l');
+
         $views = new Twig([
-            'One' => __DIR__.'/templates',
-            'Two' => __DIR__.'/another',
+            'One'   => __DIR__.'/templates',
+            'Two'   => __DIR__.'/another',
+            'Three' => [
+                __DIR__.'/multi',
+            ],
         ]);
 
         $outputOne = $views->fetch('@One/example.html', [
             'name' => 'Peter'
         ]);
 
-        $outputTwo = $views->fetch('@Two/example.html', [
+        $outputTwo = $views->fetch('@Two/another_example.html', [
             'name'   => 'Peter',
             'gender' => 'male'
         ]);
 
+        $outputThree = $views->fetch('@Three/directory/template/example.html', [
+            'weekday' => $weekday,
+        ]);
+
         $this->assertEquals("<p>Hi, my name is Peter.</p>\n", $outputOne);
         $this->assertEquals("<p>Hi, my name is Peter and I am male.</p>\n", $outputTwo);
+        $this->assertEquals('Happy Tuesday!', $outputThree);
     }
 
     public function testRender()
     {
+        $view = new Twig(dirname(__FILE__) . '/templates');
+
         $mockBody = $this->getMockBuilder('Psr\Http\Message\StreamInterface')
             ->disableOriginalConstructor()
             ->getMock();
@@ -85,7 +123,7 @@ class TwigTest extends \PHPUnit_Framework_TestCase
             ->method('getBody')
             ->willReturn($mockBody);
 
-        $response = $this->view->render($mockResponse, 'example.html', [
+        $response = $view->render($mockResponse, 'example.html', [
             'name' => 'Josh'
         ]);
         $this->assertInstanceOf('Psr\Http\Message\ResponseInterface', $response);

tests/multi/directory/template/example.html
@@ -1,0 +1,1 @@
+Happy {{ weekday }}!