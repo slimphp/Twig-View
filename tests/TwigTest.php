slimphp/Twig-View

commit 09eca0e28c7763938a450fa322691fce3d119319

Add the ability to add multiple paths with NS
Allows an array of paths with the key as a namespace. Will
detect if the type is a string and will add the path of the
template as before to not break usage.

src/Twig.php
@@ -50,12 +50,15 @@ class Twig implements \ArrayAccess
     /**
      * Create new Twig view
      *
-     * @param string $path     Path to templates directory
-     * @param array  $settings Twig environment settings
+     * @param string|array $path     Path(s) to templates directory
+     * @param array        $settings Twig environment settings
      */
     public function __construct($path, $settings = [])
     {
-        $this->loader = new \Twig_Loader_Filesystem($path);
+        $this->loader = is_string($path)
+            ? new \Twig_Loader_Filesystem($path)
+            : $this->addPaths($path);
+
         $this->environment = new \Twig_Environment($this->loader, $settings);
     }
 
@@ -102,6 +105,23 @@ class Twig implements \ArrayAccess
          $response->getBody()->write($this->fetch($template, $data));
 
          return $response;
+    }
+
+    /**
+     * Add a selection of paths with the desired namespace
+     *
+     * @param array $paths
+     * @return \Twig_Loader_Filesystem
+     */
+    private function addPaths(array $paths)
+    {
+        $loader = new \Twig_Loader_Filesystem();
+
+        foreach ($paths as $namespace => $path) {
+            $loader->addPath($path, $namespace);
+        }
+
+        return $loader;
     }
 
     /********************************************************************************

tests/TwigTest.php
@@ -33,6 +33,39 @@ class TwigTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("<p>Hi, my name is Josh.</p>\n", $output);
     }
 
+    public function testSingleTemplateWithANamespace()
+    {
+        $views = new Twig([
+            'One' => __DIR__.'/templates',
+        ]);
+
+        $output = $views->fetch('@One/example.html', [
+            'name' => 'Josh'
+        ]);
+
+        $this->assertEquals("<p>Hi, my name is Josh.</p>\n", $output);
+    }
+
+    public function testMultipleTemplatesWithMulNamespace()
+    {
+        $views = new Twig([
+            'One' => __DIR__.'/templates',
+            'Two' => __DIR__.'/another',
+        ]);
+
+        $outputOne = $views->fetch('@One/example.html', [
+            'name' => 'Peter'
+        ]);
+
+        $outputTwo = $views->fetch('@Two/example.html', [
+            'name'   => 'Peter',
+            'gender' => 'male'
+        ]);
+
+        $this->assertEquals("<p>Hi, my name is Peter.</p>\n", $outputOne);
+        $this->assertEquals("<p>Hi, my name is Peter and I am male.</p>\n", $outputTwo);
+    }
+
     public function testRender()
     {
         $mockBody = $this->getMockBuilder('Psr\Http\Message\StreamInterface')

tests/another/example.html
@@ -1,0 +1,1 @@
+<p>Hi, my name is {{ name }} and I am {{ gender }}.</p>

commit 49e2ecc94ec2ea626f32f07c4f2bfe7dc82c5a2f

Fixes #52 - Refactor `addPaths` method
Allows the use of `new Slim\Views\Twig(['path1', 'path2']);` again
as it was previously broken by 09eca0e28c7763938a450fa322691fce3d119319.

src/Twig.php
@@ -118,7 +118,11 @@ class Twig implements \ArrayAccess
         $loader = new \Twig_Loader_Filesystem();
 
         foreach ($paths as $namespace => $path) {
-            $loader->setPaths($path, $namespace);
+            if (is_string($namespace)) {
+                $loader->setPaths($path, $namespace);
+            } else {
+                $loader->addPath($path);
+            }
         }
 
         return $loader;

tests/TwigTest.php
@@ -102,6 +102,23 @@ class TwigTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('Happy Tuesday!', $outputThree);
     }
 
+    public function testMultipleDirectoriesWithoutNamespaces()
+    {
+        $weekday = (new \DateTimeImmutable('2016-03-08'))->format('l');
+        $view    = new Twig([__DIR__.'/multi/', __DIR__.'/another/']);
+
+        $rootDirectory = $view->fetch('directory/template/example.html', [
+            'weekday' => $weekday,
+        ]);
+        $multiDirectory  = $view->fetch('another_example.html', [
+            'name'   => 'Peter',
+            'gender' => 'male',
+        ]);
+
+        $this->assertEquals('Happy Tuesday!', $rootDirectory);
+        $this->assertEquals("<p>Hi, my name is Peter and I am male.</p>\n", $multiDirectory);
+    }
+
     public function testRender()
     {
         $view = new Twig(dirname(__FILE__) . '/templates');