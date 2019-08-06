387052f852e003410cc0aa4d75cd2a836f7d6eab
Zakaria <z4k4ri4@gmail.com>
2019-08-07 06:03:05 +0700

    Refactoring unit test

--- tests/LazyTwigMiddlewareTest.php
+++ tests/LazyTwigMiddlewareTest.php
@@ -9,70 +9,20 @@ declare(strict_types=1);
 
 namespace Slim\Tests;
 
-use Prophecy\Argument;
-use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use ReflectionProperty;
 use RuntimeException;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorInterface;
 use Slim\Interfaces\RouteParserInterface;
 use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
 use Slim\Views\LazyTwigMiddleware;
-use Slim\Views\TwigRuntimeExtension;
-use Slim\Views\TwigRuntimeLoader;
 
 class LazyTwigMiddlewareTest extends TestCase
 {
-    /**
-     * Create a twig prophecy given a uri prophecy and a base path.
-     *
-     * @param ObjectProphecy $uriProphecy
-     * @param string         $basePath
-     *
-     * @return ObjectProphecy
-     */
-    private function createTwigProphecy(ObjectProphecy $uriProphecy, string $basePath): ObjectProphecy
-    {
-        $self = $this;
-
-        $twigProphecy = $this->prophesize(Twig::class);
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $twigProphecy
-            ->addExtension(Argument::type('object'))
-            ->will(function ($args) use ($self) {
-                /** @var TwigExtension $extension */
-                $extension = $args[0];
-
-                $self->assertEquals('slim', $extension->getName());
-            })
-            ->shouldBeCalledOnce();
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $twigProphecy->
-        addRuntimeLoader(Argument::type('object'))
-            ->will(function ($args) use ($self, $uriProphecy, $basePath) {
-                /** @var TwigRuntimeLoader $runtimeLoader */
-                $runtimeLoader = $args[0];
-                $runtimeExtension = $runtimeLoader->load(TwigRuntimeExtension::class);
-
-                $self->assertInstanceOf(TwigRuntimeExtension::class, $runtimeExtension);
-
-                /** @var TwigRuntimeExtension $runtimeExtension */
-                $self->assertSame($uriProphecy->reveal(), $runtimeExtension->getUri());
-                $self->assertSame($basePath, $runtimeExtension->getBasePath());
-            })
-            ->shouldBeCalledOnce();
-
-        return $twigProphecy;
-    }
-
     /**
      * @expectedException RuntimeException
      * @expectedExceptionMessage The app does not have a container.
@@ -101,21 +51,10 @@ class LazyTwigMiddlewareTest extends TestCase
 
         $middleware = LazyTwigMiddleware::create($app, $containerKey);
 
-        $containerProperty = new ReflectionProperty(LazyTwigMiddleware::class, 'container');
-        $containerProperty->setAccessible(true);
-        $this->assertSame($container, $containerProperty->getValue($middleware));
-
-        $containerKeyProperty = new ReflectionProperty(LazyTwigMiddleware::class, 'containerKey');
-        $containerKeyProperty->setAccessible(true);
-        $this->assertSame($containerKey, $containerKeyProperty->getValue($middleware));
-
-        $routeParserProperty = new ReflectionProperty(LazyTwigMiddleware::class, 'routeParser');
-        $routeParserProperty->setAccessible(true);
-        $this->assertSame($routeParser, $routeParserProperty->getValue($middleware));
-
-        $basePathProperty = new ReflectionProperty(LazyTwigMiddleware::class, 'basePath');
-        $basePathProperty->setAccessible(true);
-        $this->assertSame($basePath, $basePathProperty->getValue($middleware));
+        $this->assertInaccessiblePropertySame($container, $middleware, 'container');
+        $this->assertInaccessiblePropertySame($containerKey, $middleware, 'containerKey');
+        $this->assertInaccessiblePropertySame($routeParser, $middleware, 'routeParser');
+        $this->assertInaccessiblePropertySame($basePath, $middleware, 'basePath');
     }
 
     public function testProcess()
--- tests/TwigMiddlewareTest.php
+++ tests/TwigMiddlewareTest.php
@@ -9,68 +9,18 @@ declare(strict_types=1);
 
 namespace Slim\Tests;
 
-use Prophecy\Argument;
-use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use ReflectionProperty;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorInterface;
 use Slim\Interfaces\RouteParserInterface;
 use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
 use Slim\Views\TwigMiddleware;
-use Slim\Views\TwigRuntimeExtension;
-use Slim\Views\TwigRuntimeLoader;
 
 class TwigMiddlewareTest extends TestCase
 {
-    /**
-     * Create a twig prophecy given a uri prophecy and a base path.
-     *
-     * @param ObjectProphecy $uriProphecy
-     * @param string         $basePath
-     *
-     * @return ObjectProphecy
-     */
-    private function createTwigProphecy(ObjectProphecy $uriProphecy, string $basePath): ObjectProphecy
-    {
-        $self = $this;
-
-        $twigProphecy = $this->prophesize(Twig::class);
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $twigProphecy
-            ->addExtension(Argument::type('object'))
-            ->will(function ($args) use ($self) {
-                /** @var TwigExtension $extension */
-                $extension = $args[0];
-
-                $self->assertEquals('slim', $extension->getName());
-            })
-            ->shouldBeCalledOnce();
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $twigProphecy->
-        addRuntimeLoader(Argument::type('object'))
-            ->will(function ($args) use ($self, $uriProphecy, $basePath) {
-                /** @var TwigRuntimeLoader $runtimeLoader */
-                $runtimeLoader = $args[0];
-                $runtimeExtension = $runtimeLoader->load(TwigRuntimeExtension::class);
-
-                $self->assertInstanceOf(TwigRuntimeExtension::class, $runtimeExtension);
-
-                /** @var TwigRuntimeExtension $runtimeExtension */
-                $self->assertSame($uriProphecy->reveal(), $runtimeExtension->getUri());
-                $self->assertSame($basePath, $runtimeExtension->getBasePath());
-            })
-            ->shouldBeCalledOnce();
-
-        return $twigProphecy;
-    }
-
     public function testCreate()
     {
         $twig = $this->createMock(Twig::class);
@@ -86,17 +36,9 @@ class TwigMiddlewareTest extends TestCase
 
         $middleware = TwigMiddleware::create($app, $twig);
 
-        $twigProperty = new ReflectionProperty(TwigMiddleware::class, 'twig');
-        $twigProperty->setAccessible(true);
-        $this->assertSame($twig, $twigProperty->getValue($middleware));
-
-        $routeParserProperty = new ReflectionProperty(TwigMiddleware::class, 'routeParser');
-        $routeParserProperty->setAccessible(true);
-        $this->assertSame($routeParser, $routeParserProperty->getValue($middleware));
-
-        $basePathProperty = new ReflectionProperty(TwigMiddleware::class, 'basePath');
-        $basePathProperty->setAccessible(true);
-        $this->assertSame($basePath, $basePathProperty->getValue($middleware));
+        $this->assertInaccessiblePropertySame($twig, $middleware, 'twig');
+        $this->assertInaccessiblePropertySame($routeParser, $middleware, 'routeParser');
+        $this->assertInaccessiblePropertySame($basePath, $middleware, 'basePath');
     }
 
     public function testProcess()
