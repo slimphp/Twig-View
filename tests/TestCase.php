b125037219270cc2c5e1cef033848e9f7bdbe186
Rob Allen <rob@akrabat.com>
2025-11-01 21:17:38 +0000

    Replace Prophecy with native PHPUnit mocks

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 
 namespace Slim\Tests;
 
-use Prophecy\PhpUnit\ProphecyTrait;
 use PHPUnit\Framework\TestCase as PhpUnitTestCase;
 use ReflectionProperty;
 
@@ -18,8 +17,6 @@ use function get_class;
 
 abstract class TestCase extends PhpUnitTestCase
 {
-    use ProphecyTrait;
-
     protected function assertInaccessiblePropertySame($expected, $obj, string $name)
     {
         $prop = new ReflectionProperty(get_class($obj), $name);
